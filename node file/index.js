3e99efe869c28b87f624b8261429b2fe572db0b7
devanshee79 <76983086+devanshee79@users.noreply.github.com>
2021-06-15 18:01:23 +0530

    Add files via upload

---
+++ node file/index.js
@@ -0,0 +1,63 @@
+const amount = 13;
+
+if (amount>14)
+{
+    console.log('the amount is too large');
+}
+else {                              
+    console.log('the amount is samll');
+
+}
+const fs = require("fs");
+
+fs.writeFileSync('read.txt',"welcome to my channel");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
